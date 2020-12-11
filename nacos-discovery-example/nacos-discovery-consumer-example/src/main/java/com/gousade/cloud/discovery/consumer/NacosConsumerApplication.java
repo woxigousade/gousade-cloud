08b87e1dbdeadd1e75603066ff1d36311b414443
woxigousade <woxigsd@gmail.com>
2020-12-11 17:09:18 +0800

    optimize

--- nacos-discovery-example/nacos-discovery-consumer-example/src/main/java/com/gousade/cloud/discovery/consumer/NacosConsumerApplication.java
+++ nacos-discovery-example/nacos-discovery-consumer-example/src/main/java/com/gousade/cloud/discovery/consumer/NacosConsumerApplication.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-//@ComponentScan(basePackages = {"com.gousade"})
+@ComponentScan(basePackages = {"com.gousade"})
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
